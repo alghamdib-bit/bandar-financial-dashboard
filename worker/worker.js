6046016fc4b752025d1d9a2905887ade4fe798a4
Bandar <alghamdi.b@gmail.com>
2026-02-18 19:33:18 +0300

    fix: allow X-Dashboard-Secret in CORS preflight headers

--- worker/worker.js
+++ worker/worker.js
@@ -18,7 +18,7 @@
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Headers': 'Content-Type, X-Dashboard-Secret',
   'Content-Type': 'application/json',
 };
 
